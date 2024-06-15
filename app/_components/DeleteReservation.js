93eb81079484c0afd34290289d899c887ac3ab87
imEmily-83 <ali.emily.1383@gmail.com>
2024-06-15 09:20:47 +0330

    add fniall parts and features

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -5,18 +5,17 @@ import { useTransition } from 'react';
 import Spinner from './Spinner';
 
 function DeleteReservation({ bookingId , onDelete }) {
-  const [isPending ,  startTransition]  = useTransition();
 
   function handleDelete(){
     if(confirm("Are you sure to delete Reservation ?")){
-      startTransition(()=> onDelete(bookingId))
+      onDelete(bookingId)
     }
   }
   return (
     
-    <button onClick={handleDelete} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
+    <button onClick={onDelete} className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
     
-    { isPending ? <Spinner/> : <>
+    {      <>
     <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
        <span className='mt-1'>Delete</span>
     </> }
